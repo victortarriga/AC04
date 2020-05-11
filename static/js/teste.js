2ca74a98fc301198586d563f3189fc5a747c6efb
victortarriga <victor.tarriga@gmail.com>
2020-05-11 17:15:20 -0300

    AC

--- static/js/teste.js
+++ static/js/teste.js
@@ -18,7 +18,7 @@ for (var i = 1; i <= 10; i++) {
     c.appendChild(t);
 
     var p = document.createElement('p');
-    p.textContent = 'DALE DELE DOly dale dele doly dale dele dlyl dale dely';
+    p.textContent = 'blá blá blá blá blá blá blá blá blá blá blá blá';
     c.appendChild(p);
 }
 
